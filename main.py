4d99ca5544a8cfca7db6d1eb03f5be8f572ecc09
Adesola Busari <adesolaisa3@gmail.com>
2025-01-30 00:06:51 +0100

    second commit

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,session
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_required, login_user
+from flask_login import LoginManager, UserMixin, login_required, login_user, current_user, logout_user
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
 import datetime
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///blog.db"
@@ -57,13 +56,17 @@ def load_user(user_id):
     return Users.query.get(user_id)
 
 
+year = datetime.datetime.now().year
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    blogs = Posts.query.all()
+    return render_template('index.html', blogs = blogs, current_year=year)
 
 
 @app.route('/signup',methods=["POST","GET"])
 def signup():
+
     if request.method == "POST":
         username = request.form.get("username")
         email = request.form.get("email")
@@ -80,7 +83,7 @@ def signup():
         db.session.commit()
         print("three")
         return redirect(url_for('login'))
-    return render_template('signup.html')
+    return render_template('signup.html', current_year=year)
 
 @app.route('/login', methods= ["POST", "GET"])
 def login():
@@ -95,7 +98,12 @@ def login():
         else:
             return ("Invalid Email or password")
 
-    return render_template('login.html')
+    return render_template('login.html', current_year=year)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
 
 @app.route('/post', methods=["POST", "GET"])
 @login_required
@@ -104,22 +112,62 @@ def post():
         title= request.form.get("title")
         content = request.form.get("content")
         author = request.form.get("author")
+        user_id= current_user.id
 
         title_check = Posts.query.filter_by(title = title).first()
         if title_check:
             return (f" A post with {title} already exists")
         else:
-            new_post = Posts(title = title, content= content, author= author)
+            new_post = Posts(title = title, content= content, author= author, user_id = user_id)
             db.session.add(new_post)
             db.session.commit()
-    return render_template('post.html')
+            return redirect(url_for("home"))
+    return render_template('post.html', current_year=year)
 
 @app.route('/blogs', methods =['GET', 'POST'])
 def blogs():
     blogs = Posts.query.all()
+    print(blogs)
+    return render_template("view.html", blogs=blogs)
 
-    return render_template("view.html", blogs)
+@app.route('/profile', methods = ["GET", "POST"])
+@login_required
+def profile():
+    user_id = current_user.id
+    user_info = Users.query.filter_by(id=user_id).first()
+    user_posts = Posts.query.filter_by(user_id=user_id).all()
+    return render_template("dashboard.html", user_info=user_info, user_posts=user_posts, current_year= year)
 
+
+@app.route('/edit/<int:post_id>', methods=["GET","POST"])
+@login_required
+def edit(post_id):
+    post_to_edit = Posts.query.get_or_404(post_id)
+    if request.method== "POST":
+        title = request.form.get("title")
+        content = request.form.get("content")
+
+        if title:
+            post_to_edit.title = title
+        if content:
+            post_to_edit.content = content
+        else:
+            post_to_edit.title= title
+            post_to_edit.content = content
+        db.session.commit()
+        return redirect(url_for("profile"))
+    return render_template("edit.html", posts = post_to_edit)
+        
+
+
+@app.route('/delete', methods= ["GET","POST"])
+def delete():
+    psot_id = request.args.get("post_id")
+    post_to_delete = Posts.query.filter_by(id=psot_id).first()
+    if post_to_delete:
+        db.session.delete(post_to_delete)
+        db.session.commit()
+        return redirect(url_for('profile'))
 
 if __name__ == '__main__':
     app.run(debug=True)
